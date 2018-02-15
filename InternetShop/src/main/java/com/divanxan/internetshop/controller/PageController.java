16c333c71f81d2fa02da5bb8db4f89e57e77c803
Divan_PK\Dmitry <redemer@rambler.ru>
2018-02-15 07:13:51 +0300

    15.02.2018 7-13

--- InternetShop/src/main/java/com/divanxan/internetshop/controller/PageController.java
+++ InternetShop/src/main/java/com/divanxan/internetshop/controller/PageController.java
@@ -1,17 +1,41 @@
 package com.divanxan.internetshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
 public class PageController {
 
-    @RequestMapping(value = {"/","home", "index"})
-    public ModelAndView index(){
+    @RequestMapping(value = {"/", "home", "index"})
+    public ModelAndView index() {
         ModelAndView mv = new ModelAndView("page");
         mv.addObject("greeting", "Добро пожаловать в spring MVC");
         return mv;
     }
+/*
+    @RequestMapping(value = "/test")
+    public ModelAndView test(@RequestParam(value="greeting", required = false)String greeting ){
+        if(greeting==null){
+            greeting = "Привет!";
+        }
+        ModelAndView mv = new ModelAndView("page");
+        mv.addObject("greeting", greeting);
+        return mv;
+    }
+
+    @RequestMapping(value = "/test/{greeting}")
+    public ModelAndView test(@PathVariable(value="greeting", required = false)String greeting ){
+        if(greeting==null){
+            greeting = "Привет!";
+        }
+        ModelAndView mv = new ModelAndView("page");
+        mv.addObject("greeting", greeting);
+        return mv;
+    }
+ */
 }
+
